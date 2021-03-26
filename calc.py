2edb6a4862e3c47713b4f1a57506f5f40c7700d7
Lydmila <milayamila94@gmail.com>
2021-03-27 04:02:29 +0500

    my first commit

---
+++ calc.py
@@ -0,0 +1,24 @@
+﻿#почему == а не is?
+#а можно без километрового elif?
+#считается ли обработкой некорректного результата ошибка, если первое значение - не число, а какой-то бред?
+#считается ли обработкой некорректного результата ошибка, если делим на 0?
+result = int(input ('vvedite pervoe chislo '))
+if result is int:
+    print("ок")
+m = 0
+while m!='=':
+    m = input ('vvedite operatsiy i chislo ')
+    if m!='=':
+        m.split(',')
+        m0 = m[0]
+        m1 = int(m[1:])
+        if m0 == '+':
+            result += m1
+        elif m0 == '-':
+            result -= m1
+        elif m0 == '/':
+            result /= m1
+        elif m0 == '*':
+            result *= m1
+        else: print('bred kakoy-to')
+else: print(result)
---
+++ main.py
@@ -0,0 +1,25 @@
+value = str (input('vvedi stroky '))
+# k = 0
+# l = 0
+# m = 0
+# p = 0
+# s = 0
+# r = 0
+# for i in value:
+#     if i in "a":
+#         k = k + 1
+#     elif i in "o":
+#         l = l + 1
+#     elif i in "e":
+#         m = m + 1
+#     elif i in "i":
+#         p = p + 1
+#     elif i in "u":
+#         s = s + 1
+#     elif i in "y":
+#         r = r + 1
+# print('a - ', k, 'o - ', l, 'e - ', m, 'i - ', p, 'u - ', s, 'y - ', r)
+print("a - ", value.count("a"))
+print(value.count("e"))
+print(value.count("o"))
+
